cf4984998f3951e39ae62c320969831816a347d7
sjsu-koby <koby.nguyen@sjsu.edu>
2022-04-17 20:52:45 -0700

    imported image and changes to main

--- framework/main.js
+++ framework/main.js
@@ -1,4 +1,15 @@
+var objLayer;
+
+function preload(){
+  player = loadImage('images/pixeldog.png');
+
+}
+
 function setup(){
   createCanvas(windowWidth, windowHeight);
   background(0, 238, 255);
+}
+
+function draw(){
+
 }
